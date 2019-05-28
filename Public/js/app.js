650b43c122523142f510115e4314c8d84243835d
unknown <shouvikch1997@gmail.com>
2019-05-28 19:38:24 +0530

    Loading css

--- Public/js/app.js
+++ Public/js/app.js
@@ -2,21 +2,22 @@
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const message = document.querySelector('#message-0')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
-
+message.style.display="none"
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-
-    messageOne.textContent = 'Loading...'
+    message.style.display="block"
+    messageOne.textContent = ''
     messageTwo.textContent = ''
 
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                messageOne.textContent = data.error
+                message.style.display="none"
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
